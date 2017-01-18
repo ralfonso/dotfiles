6eba4e32a1adf8f4c80eeeb1b3ce7fa4eb33d69f
Ryan Roemmich <ralfonso@gmail.com>
2017-01-17 18:12:43 -0800

    polybar script fix for single head. temp overline fix

--- i3/.i3/bars.sh
+++ i3/.i3/bars.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
 pkill -f polybar
-~/.local/bin/polybar main &
-~/.local/bin/polybar left &
-~/.local/bin/polybar right &
+
+display_count=$(xrandr | grep -c ' connected')
+
+if [[ "${display_count}" -eq 1 ]];
+then
+  ~/.local/bin/polybar main &
+else
+  ~/.local/bin/polybar main &
+  ~/.local/bin/polybar left &
+  ~/.local/bin/polybar right &
+fi
